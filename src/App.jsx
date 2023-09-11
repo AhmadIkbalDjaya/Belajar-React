8f455f3056338bcab5d7d4abb28366ce5b589819
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-09-11 12:51:56 +0700

    3. Components & Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+const Button = (props) => {
+  const {children = "...", variant="bg-black"} = props;
+  return (
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
 
 export default function App() {
   return (
-    <div className="flex bg-blue-600">App</div>
-  )
+    <div className="flex justify-center min-h-screen bg-blue-600 items-center gap-x-5">
+      <Button variant="bg-blue-700"> 
+        Login
+      </Button>
+      <Button variant="bg-red-700"> 
+        Logout
+      </Button>
+      <Button></Button>
+    </div>
+  );
 }
