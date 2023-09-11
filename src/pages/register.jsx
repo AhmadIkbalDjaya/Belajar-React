d195851a9ac4d3d89077e769b54344310fcef4e3
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-09-11 22:07:18 +0700

    06. React Router

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,17 @@
 import AuthLayouts from "../components/Layouts/AuthLayouts";
 import FormRegister from "../components/Fragments/FormRegister";
+import { Link } from "react-router-dom";
 
 export default function RegisterPage() {
   return (
     <AuthLayouts title="Register">
       <FormRegister />
+      <p className="text-sm mt-5 text-center">
+        Have an account?{" "}
+        <Link to={"/login"} className="font-bold text-blue-600">
+          Login
+        </Link>
+      </p>
     </AuthLayouts>
   );
 }
