2fe32b8dc2d19d1ef8d1ed8673b63f2e1a4aeb72
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-09-12 14:09:59 +0700

    09. Rendering List

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,27 +1,42 @@
-import React from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    price: "1.000.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, consequuntur aperiam? Voluptatum nobis inventore excepturi nemo voluptates quos ullam fugiat accusantium cupiditate maiores deserunt ipsam, quae totam explicabo obcaecati rerum.`,
+  },
+  {
+    id: 2,
+    name: "Sepatu Lama",
+    price: "100.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, consequuntur aperiam? Voluptatum nobis inventore.`,
+  },
+  {
+    id: 3,
+    name: "Sepatu Lagi",
+    price: "2.000.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, consequuntur aperiam? Voluptatum`,
+  },
+];
 export default function ProductsPage() {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo odio
-          vero, sit quae dolore error, mollitia cum incidunt ratione repellat
-          eos quos. Suscipit officiis nemo eos corrupti ea voluptatibus eveniet?
-        </CardProduct.Body>
-        <CardProduct.Footer price="1.000.000" />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo odio
-          vero, sit quae dolore error, mollitia cum incidunt ratione repellat
-          eos quos. Suscipit officiis nemo eos corrupti ea voluptatibus eveniet?
-        </CardProduct.Body>
-        <CardProduct.Footer price="1.000.000" />
-      </CardProduct>
+      {products.map((product) => {
+        return (
+          <CardProduct key={product.id}>
+            <CardProduct.Header image={product.image} />
+            <CardProduct.Body name={product.name}>
+              {product.description}
+            </CardProduct.Body>
+            <CardProduct.Footer price={product.price} />
+          </CardProduct>
+        );
+      })}
     </div>
   );
 }
