efa14ae3c8b4f2ee081d2b1711e49f7d29f906f3
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-09-15 10:14:54 +0700

    14. useEffect

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import Button from "../components/Elements/Button";
 import Counter from "../components/Fragments/Counter";
@@ -26,14 +26,28 @@ const products = [
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, consequuntur aperiam? Voluptatum`,
   },
 ];
+
 const email = localStorage.getItem("email");
+
 export default function ProductsPage() {
-  const [cart, setCart] = useState([
-    {
-      id: 1,
-      qty: 1,
-    },
-  ]);
+  const [cart, setCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    // setCart([{ id: 1, qty: 1 }]);
+    setCart(JSON.parse(localStorage.getItem("cart")) || []);
+  }, []);
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      const sum = cart.reduce((acc, item) => {
+        const product = products.find((product) => product.id === item.id);
+        return acc + product.price * item.qty;
+      }, 0);
+      setTotalPrice(sum);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }, [cart]);
 
   const handleLogout = () => {
     localStorage.removeItem("email");
@@ -51,13 +65,6 @@ export default function ProductsPage() {
     } else {
       setCart([...cart, { id, qty: 1 }]);
     }
-    // setCart([
-    //   ...cart,
-    //   {
-    //     id,
-    //     qty: 1,
-    //   },
-    // ]);
   };
   return (
     <Fragment>
@@ -122,12 +129,23 @@ export default function ProductsPage() {
                   </tr>
                 );
               })}
+              <tr>
+                <td colSpan={3}>
+                  <b>Total Price</b>
+                </td>
+                <td>
+                  <b>
+                    Rp.{" "}
+                    {totalPrice.toLocaleString("id-ID", {
+                      styles: "currency",
+                      currency: "IDR",
+                    })}
+                  </b>
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
-      </div>
-      <div className="my-5 flex justify-center">
-        <Counter></Counter>
       </div>
     </Fragment>
   );
