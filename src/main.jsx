41de9b4ae2b786f240b3a31f0896413554c24259
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-09-16 21:10:29 +0700

    18. Custom Hooks

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import LoginPage from "./Pages/Login.jsx";
 import RegisterPage from "./Pages/register.jsx";
 import ErrorPage from "./pages/error";
 import ProductsPage from "./pages/products";
+import ProfilePage from "./pages/profile";
 
 const router = createBrowserRouter([
   {
@@ -23,8 +24,12 @@ const router = createBrowserRouter([
   },
   {
     path: "/products",
-    element: <ProductsPage />
-  }
+    element: <ProductsPage />,
+  },
+  {
+    path: "/profile",
+    element: <ProfilePage />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
